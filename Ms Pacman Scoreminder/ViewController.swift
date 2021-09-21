42341128c6fa7c7f42b78379ae5908a220ed31f2
Aaron Nance <apnance@hotmail.com>
2021-09-21 10:23:30 -0700

    Initial Commit

---
+++ Ms Pacman Scoreminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ms Pacman Scoreminder
+//
+//  Created by Aaron Nance on 9/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Ms Pacman ScoreminderTests/Ms_Pacman_ScoreminderTests.swift
@@ -0,0 +1,33 @@
+//
+//  Ms_Pacman_ScoreminderTests.swift
+//  Ms Pacman ScoreminderTests
+//
+//  Created by Aaron Nance on 9/21/21.
+//
+
+import XCTest
+@testable import Ms_Pacman_Scoreminder
+
+class Ms_Pacman_ScoreminderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
