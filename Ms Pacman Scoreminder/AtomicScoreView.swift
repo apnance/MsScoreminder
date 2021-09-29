97f1af6bb6c61ff714689f62194240b269044a7e
Aaron Nance <apnance@hotmail.com>
2021-09-29 15:54:51 -0700

    Refactor: add factory method for instantiating AtomicScoreViews from nib

--- Ms Pacman Scoreminder/AtomicScoreView.swift
+++ Ms Pacman Scoreminder/AtomicScoreView.swift
@@ -9,27 +9,40 @@ import APNUtils
 
 class AtomicScoreView: UIView {
     
+    private static let nib = UINib(nibName: "AtomicScoreView",
+                                   bundle: Bundle.main)
+    
+    /// Factory method for instantiating AtomicScoreViews via nib loading.
+    static func new(delegate: AtomicScoreViewDelegate?,
+                    withScore score: Score) -> AtomicScoreView {
+        
+        let scoreView = nib.instantiate(withOwner: self,
+                                        options: nil).first as! AtomicScoreView
+        scoreView.delegate = delegate
+        scoreView.load(score: score)
+        
+        return scoreView
+        
+    }
+    
     @IBOutlet var scoreView: UILabel!
     @IBOutlet var dateView: UILabel!
     @IBOutlet var fruitView: UIImageView!
     @IBOutlet weak var borderView: UIView!
     
-    var delegate: AtomicScoreViewDelegate?
-    var score: Score!
-    
-    private var nibName: String { "AtomicScoreView" }
-    
-    func load(score: Score) {
+    private var delegate: AtomicScoreViewDelegate?
+    private var score: Score!
+        
+    private func load(score: Score) {
     
         self.score = score
-        
-        scoreView.text = score.score.delimited
-        dateView.text = score.date.simple
+        scoreView.text  = score.score.delimited
+        dateView.text   = score.date.simple
         
         fruitView.image = score.levelIconImage
         
-        layer.cornerRadius = frame.height / 5.0
-        clipsToBounds = true
+        layer.cornerRadius              = frame.height / 5.0
+        clipsToBounds                   = true
         borderView.layer.cornerRadius   = frame.height / 5.0
         borderView.layer.borderWidth    = frame.height / 10.0
         borderView.layer.borderColor    = score.levelColor.cgColor
