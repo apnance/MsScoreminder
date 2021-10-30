a27c162fce67fa98b9dd3046a807c087c923d2c5
Aaron Nance <apnance@hotmail.com>
2021-10-29 19:16:40 -0700

    Add

---
+++ Ms Pacman Scoreminder/DailyStats.swift
@@ -0,0 +1,18 @@
+//
+//  DailyStats.swift
+//  Ms Scoreminder
+//
+//  Created by Aaron Nance on 10/29/21.
+//
+
+import Foundation
+
+struct DailyStats {
+    
+    var date: Date = Date()
+    var rank: (Int, Int) = (-123,-1234)
+    var averageScore: Int = -12345
+    var gameCount: Int = -12345
+    
+}
+
