apnance/MsScoreminder

commit 78fb912fdd5dd66e68e4503fd900ede0478016cb

Features:	1) add percentile display to AtomicScoreView 		2) add visual cycling of data for AtomicScoreView

Ms Pacman Scoreminder/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController {
     private var scoreToDelete: Score?
     private let (w, h, p) = (100.0, 50.0, 10.0)
     
+    
+    private var scoreCycler  = (num: -34,
+                                incr: 1,
+                                dataCurrent: 1,
+                                dataMax: 0)
+    
+    private var scoreViews = [AtomicScoreView]()
+    private var timer: APNTimer?
+    
     // MARK: - Outlets
     @IBOutlet var mainView: UIView!
     @IBOutlet weak var backgroundStripeView: UIView!
@@ -75,7 +84,48 @@ class ViewController: UIViewController {
         uiScoreInput()
         uiBuildLevelSelector()
         
+        cycleScores()
+        
         uiVolatile()
+        
+    }
+    
+    private func cycleScores() {
+
+        if timer == nil {
+            
+            timer = APNTimer(name: "scores",
+                             repeatInterval: 0.06) {
+                
+                _ in
+                
+                self.cycleScores()
+                
+            }
+            
+            scoreCycler.dataMax = scoreMan.getDataCount() - 1
+            
+            return /*EXIT*/
+            
+        }
+        
+        if scoreCycler.num > 34 {
+            
+            scoreCycler.incr        = -1
+            scoreCycler.dataCurrent = scoreCycler.dataCurrent >= scoreCycler.dataMax ? 0 : scoreCycler.dataCurrent + 1
+            
+        } else if scoreCycler.num < -16 {
+            
+            scoreCycler.incr        = 1
+            scoreCycler.dataCurrent = scoreCycler.dataCurrent >= scoreCycler.dataMax ? 0 : scoreCycler.dataCurrent + 1
+            
+        } else if scoreCycler.num >= 0 && scoreCycler.num <= scoreViews.lastUsableIndex {
+        
+            scoreViews[scoreCycler.num].updateDisplay(useData: scoreCycler.dataCurrent)
+            
+        }
+        
+        scoreCycler.num += scoreCycler.incr
         
     }
     
@@ -187,12 +237,18 @@ class ViewController: UIViewController {
         
         var (xO, yO) = ((scoresView.frame.width / colCount.double) / 2.0, (h + p) * 0.55)
         
+        scoreViews = []
         scoresView.removeAllSubviews()
+        scoreViews = []
+        
         
         for score in scores {
             
             let scoreView = AtomicScoreView.new(delegate: self,
-                                                withScore: score, andRank: scoreMan.getRank(score))
+                                                withScore: score,
+                                                andData: scoreMan.getData(score))
+            
+            scoreViews.append(scoreView)
             
             scoreView.translatesAutoresizingMaskIntoConstraints = true
             scoresView.addSubview(scoreView)
@@ -415,7 +471,7 @@ extension ViewController: AtomicScoreViewDelegate {
         
         let scoreView = AtomicScoreView.new(delegate: nil,
                                             withScore: score,
-                                            andRank: scoreMan.getRank(score))
+                                            andData: scoreMan.getData(score))
         
         deleteScoreContainerView.removeAllSubviews()
         deleteScoreContainerView.translatesAutoresizingMaskIntoConstraints = true

Ms Pacman Scoreminder/AtomicScoreView.swift
@@ -15,17 +15,17 @@ class AtomicScoreView: UIView {
     /// Factory method for instantiating AtomicScoreViews via nib loading.
     static func new(delegate: AtomicScoreViewDelegate?,
                     withScore score: Score,
-                    andRank rank: Int) -> AtomicScoreView {
+                    andData data: [String]) -> AtomicScoreView {
         
         let scoreView = nib.instantiate(withOwner: self,
                                         options: nil).first as! AtomicScoreView
         scoreView.delegate = delegate
-        scoreView.load(score: score, rank: rank)
+        scoreView.load(score: score, data: data)
         
         return scoreView
         
     }
-    
+        
     @IBOutlet var scoreView: UILabel!
     @IBOutlet var dateView: UILabel!
     @IBOutlet var fruitView: UIImageView!
@@ -35,27 +35,25 @@ class AtomicScoreView: UIView {
     
     private var delegate: AtomicScoreViewDelegate?
     private var score: Score!
+            
+    private var data = [String]()
+  
+    private func load(score: Score, data: [String]) {
         
-    private func load(score: Score, rank: Int) {
-    
+        self.data = data
+        
         self.score = score
         scoreView.text  = score.score.delimited
         dateView.text   = score.date.simple
         fruitView.image = score.levelIcon
         
         layer.cornerRadius              = frame.height / 5.0
-        clipsToBounds                   = true
-        borderView.layer.cornerRadius   = frame.height / 5.0
-        borderView.layer.borderWidth    = frame.height / 10.0
-        borderView.layer.borderColor    = score.levelForeColor.cgColor
         
-        rotateRandom(minRange: -1.0...(-0.5), maxRange: 0.5...1.0)
+        borderView.layer.cornerRadius   = frame.height / 5.0
+        borderView.layer.borderWidth    = frame.height / 8.0
         
         gameRank.rotate(angle: -22.5)
-        gameRank.text = rank.description
-        gameRank.textColor = score.levelBackColor
-        gameRankBG.backgroundColor = score.levelForeColor
-        
+
         addShadows()
         
         // Interaction
@@ -64,13 +62,15 @@ class AtomicScoreView: UIView {
         
         self.addGestureRecognizer(tap)
         
+        updateDisplay()
+        
     }
     
     @objc func handleTap() { delegate?.didTap(score: score) }
     
     private func addShadows() {
         
-        let views = [gameRankBG!]
+        let views = [gameRankBG!, fruitView!]
         
         for view in views {
             
@@ -82,10 +82,23 @@ class AtomicScoreView: UIView {
         
     }
     
+    func updateDisplay(useData i: Int = 0) {
+        
+        gameRank.text = data[i]
+        
+        gameRank.textColor              = i % 2 != 0 ? score.colorLight : score.colorDark
+        gameRankBG.backgroundColor      = i % 2 != 0 ? score.colorDark : score.colorLight
+        
+        borderView.layer.borderColor    = gameRankBG.backgroundColor?.cgColor
+        
+        rotateRandom(minRange: -1.0...(-0.5), maxRange: 0.5...1.0)
+        
+    }
+    
 }
 	
 protocol AtomicScoreViewDelegate {
     
     func didTap(score: Score)
-        
+    
 }

Ms Pacman Scoreminder/Score.swift
@@ -55,8 +55,8 @@ struct Score {
     
     var displayScore: String { score.delimited }
     var levelString: String { Score.stringFor(level: level) }
-    var levelForeColor: UIColor { Score.colorFor(level: level) }
-    var levelBackColor: UIColor {
+    var colorLight: UIColor { Score.colorFor(level: level) }
+    var colorDark: UIColor {
         
         switch level {
             case 0,1,3,4 : return UIColor.white

Ms Pacman Scoreminder/ScoreManager.swift
@@ -178,16 +178,47 @@ struct ScoreManager {
     
     }
     
-    func getRank(_ score: Score) -> Int {
-        
-        
+    // TODO: Clean Up - rename getDataCount -> getStatCount
+    /// Returns the number of elements in the `[String]` returned by `getStats(:)`
+    func getDataCount() -> Int { getData(scores.first!).count }
+    
+    // TODO: Clean Up - rename getData -> getStats
+    /// Returns a `[String]` of ready to display score stats.
+    func getData(_ score: Score) -> [String] {
+                
         for (i, data) in scoresHighSorted.enumerated() {
             
-            if score == data { return i + 1 /*EXIT*/ }
+            if score == data {
+                
+            
+                let scoreCount = scores.count
+                let rank = i + 1
+                var percentile = String()
+                
+                switch rank {
+                    
+                case 1:
+                    
+                    percentile = " ★ "
+                    
+                case scoreCount:
+                    
+                    percentile = "LOW"
+                    
+                default:
+                    
+                    percentile = (((scoreCount - i).double / scoreCount.double) * 100).roundTo(1).description.rTrimTo(4)
+                    percentile = "\(percentile)%"
+                    
+                }
+                
+                return [rank.description, percentile] /*EXIT*/
+                
+            }
             
         }
         
-        return -1 /*EXIT*/
+        return []
         
     }
     

commit b480b7f3b27dc33f145705c38cd954ecdf826cdb

Refactor [Warning]: remove redundant initialization of scoreViews.
NOTES:
- This apparently redundant call seems deliberate and may have been done for a reason I don't comprehend.  Be warned removing this may have unintended side-effects.
- This bug has was introduced in commit 78fb912 on 10/20/21

Ms Pacman Scoreminder/ViewController.swift
@@ -583,12 +583,11 @@ class ViewController: UIViewController {
         let scoreCount  = colCount * rowCount
         let scores      = statMan.filter(count: scoreCount)
         
-        var (xO, yO) = ((scoresView.frame.width / colCount.double) / 2.0, (h + p) * 0.55)
+        var (xO, yO)    = ((scoresView.frame.width / colCount.double) / 2.0, (h + p) * 0.55)
         
-        // TODO: Clean Up - why emptying scoreViews 2x here?
-        scoreViews = []
+        // Remove old Score UI
+        scoreViews      = []
         scoresView.removeAllSubviews()
-        scoreViews = []
         
         var currentDate = Date(timeIntervalSince1970: 0).simple
         var textColor   = Configs.UI.Display.defaultAtomicScoreViewTextColor