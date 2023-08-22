2f7c9d0ff405ba283754cd40a6503a1180c6bf8f
Aaron Nance <apnance@hotmail.com>
2023-08-22 13:25:34 -0700

    Feature: add haptic response to UISegmentControl value change events

---
+++ Ms Pacman Scoreminder/UISegmentedControl.swift
@@ -0,0 +1,31 @@
+//
+//  UISegmentedControl.swift
+//  Ms Scoreminder
+//
+//  Created by Aaron Nance on 8/22/23.
+//
+
+import UIKit
+import APNUtil
+
+extension UISegmentedControl {
+
+    override open func didMoveToSuperview() {
+        
+        super.didMoveToSuperview()
+        
+        addTarget(self,
+                  action: #selector(addHaptic),
+                  for: .valueChanged)
+        
+    }
+    
+    @objc private func addHaptic() {
+        
+        haptic(withStyle: .light)
+        
+        print("\(#function)") // TODO: DELETE THIS
+        
+    }
+    
+}
