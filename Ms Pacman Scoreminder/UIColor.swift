37a3726f366d115c6f44b548754337a8bd3e8e09
Aaron Nance <apnance@hotmail.com>
2023-10-20 13:20:22 -0700

    Add

---
+++ Ms Pacman Scoreminder/UIColor.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor.swift
+//  Ms Scoreminder
+//
+//  Created by Aaron Nance on 10/20/23.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let msWhite        = UIColor.white
+    static let msBlue         = UIColor(named: "Blue")!
+    static let msPink         = UIColor(named: "Pink")!
+    static let cherry       = UIColor(named: "Cherry")!
+    static let strawberry   = UIColor(named: "Strawberry")!
+    static let orange       = UIColor(named: "Orange")!
+    static let pretzel      = UIColor(named: "Pretzel")!
+    static let apple        = UIColor(named: "Apple")!
+    static let pear         = UIColor(named: "Pear")!
+    static let banana       = UIColor(named: "Banana")!
+    
+}
